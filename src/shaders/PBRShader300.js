850cc207bd37305aec0444928f4eafd9c894f87c
nahkranoth <joey@nahkranoth.nl>
2019-06-09 17:03:41 +0200

    Added and trimmed the PBR shader

---
+++ src/shaders/PBRShader300.js
@@ -0,0 +1,143 @@
+const vertex = `#version 300 es
+precision highp float;
+precision highp int;
+in vec3 position;
+in vec2 uv;
+in vec3 normal;
+uniform mat3 normalMatrix;
+uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+out vec2 vUv;
+out vec3 vNormal;
+out vec3 vMPos;
+void main() {
+    vUv = uv;
+    vNormal = normalize(normalMatrix * normal);
+    vec4 mPos = modelMatrix * vec4(position, 1.0);
+    vMPos = mPos.xyz / mPos.w;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;
+
+const fragment = `#version 300 es
+precision highp float;
+precision highp int;
+uniform vec3 cameraPosition;
+uniform mat4 viewMatrix;
+uniform sampler2D tBaseColor;
+uniform vec3 uBaseColor;
+uniform float uAlpha;
+uniform sampler2D tRMO;
+uniform float uMetallic;
+uniform float uRoughness;
+uniform float uOcclusion;
+uniform sampler2D tNormal;
+uniform float uNormalScale;
+uniform float uNormalUVScale;
+uniform sampler2D tEmissive;
+uniform float uEmissive;
+uniform vec3 uLightDirection;
+uniform vec3 uLightColor;
+in vec2 vUv;
+in vec3 vNormal;
+in vec3 vMPos;
+out vec4 FragColor;
+const float PI = 3.14159265359;
+const float RECIPROCAL_PI = 0.31830988618;
+const float RECIPROCAL_PI2 = 0.15915494;
+const float LN2 = 0.6931472;
+const float ENV_LODS = 6.0;
+vec4 SRGBtoLinear(vec4 srgb) {
+    vec3 linOut = pow(srgb.xyz, vec3(2.2));
+    return vec4(linOut, srgb.w);;
+}
+vec4 RGBMToLinear(in vec4 value) {
+    float maxRange = 6.0;
+    return vec4(value.xyz * value.w * maxRange, 1.0);
+}
+vec3 linearToSRGB(vec3 color) {
+    return pow(color, vec3(1.0 / 2.2));
+}
+vec3 getNormal() {
+    vec3 pos_dx = dFdx(vMPos.xyz);
+    vec3 pos_dy = dFdy(vMPos.xyz);
+    vec2 tex_dx = dFdx(vUv);
+    vec2 tex_dy = dFdy(vUv);
+    vec3 t = normalize(pos_dx * tex_dy.t - pos_dy * tex_dx.t);
+    vec3 b = normalize(-pos_dx * tex_dy.s + pos_dy * tex_dx.s);
+    mat3 tbn = mat3(t, b, normalize(vNormal));
+    vec3 n = texture(tNormal, vUv * uNormalUVScale).rgb * 2.0 - 1.0;
+    n.xy *= uNormalScale;
+    vec3 normal = normalize(tbn * n);
+    // Get world normal from view normal (normalMatrix * normal)
+    return normalize((vec4(normal, 0.0) * viewMatrix).xyz);
+}
+vec3 specularReflection(vec3 specularEnvR0, vec3 specularEnvR90, float VdH) {
+    return specularEnvR0 + (specularEnvR90 - specularEnvR0) * pow(clamp(1.0 - VdH, 0.0, 1.0), 5.0);
+}
+float geometricOcclusion(float NdL, float NdV, float roughness) {
+    float r = roughness;
+    float attenuationL = 2.0 * NdL / (NdL + sqrt(r * r + (1.0 - r * r) * (NdL * NdL)));
+    float attenuationV = 2.0 * NdV / (NdV + sqrt(r * r + (1.0 - r * r) * (NdV * NdV)));
+    return attenuationL * attenuationV;
+}
+float microfacetDistribution(float roughness, float NdH) {
+    float roughnessSq = roughness * roughness;
+    float f = (NdH * roughnessSq - NdH) * NdH + 1.0;
+    return roughnessSq / (PI * f * f);
+}
+vec2 cartesianToPolar(vec3 n) {
+    vec2 uv;
+    uv.x = atan(n.z, n.x) * RECIPROCAL_PI2 + 0.5;
+    uv.y = asin(n.y) * RECIPROCAL_PI + 0.5;
+    return uv;
+}
+
+void main() {
+    vec3 baseColor = SRGBtoLinear(texture(tBaseColor, vUv)).rgb * uBaseColor;
+    // RMO map packed as rgb = [roughness, metallic, occlusion]
+    vec4 rmaSample = texture(tRMO, vUv);
+    float roughness = clamp(rmaSample.r * uRoughness, 0.04, 1.0);
+    float metallic = clamp(rmaSample.g * uMetallic, 0.04, 1.0);
+    vec3 f0 = vec3(0.04);
+    vec3 diffuseColor = baseColor * (vec3(1.0) - f0) * (1.0 - metallic);
+    vec3 specularColor = mix(f0, baseColor, metallic);
+    vec3 specularEnvR0 = specularColor;
+    vec3 specularEnvR90 = vec3(clamp(max(max(specularColor.r, specularColor.g), specularColor.b) * 25.0, 0.0, 1.0));
+    vec3 N = getNormal();
+    vec3 V = normalize(cameraPosition - vMPos);
+    vec3 L = normalize(uLightDirection);
+    vec3 H = normalize(L + V);
+    vec3 reflection = normalize(reflect(-V, N));
+    float NdL = clamp(dot(N, L), 0.001, 1.0);
+    float NdV = clamp(abs(dot(N, V)), 0.001, 1.0);
+    float NdH = clamp(dot(N, H), 0.0, 1.0);
+    float LdH = clamp(dot(L, H), 0.0, 1.0);
+    float VdH = clamp(dot(V, H), 0.0, 1.0);
+    vec3 F = specularReflection(specularEnvR0, specularEnvR90, VdH);
+    float G = geometricOcclusion(NdL, NdV, roughness);
+    float D = microfacetDistribution(roughness, NdH);
+    vec3 diffuseContrib = (1.0 - F) * (diffuseColor / PI);
+    vec3 specContrib = F * G * D / (4.0 * NdL * NdV);
+    
+    // Shading based off lights
+    vec3 color = NdL * uLightColor * (diffuseContrib + specContrib);
+    // Get base alpha
+    float alpha = 1.0;
+    // Add lights spec to alpha for reflections on transparent surfaces (glass)
+    alpha = max(alpha, max(max(specContrib.r, specContrib.g), specContrib.b));
+    // Multiply occlusion
+    color = mix(color, color * rmaSample.b, uOcclusion);
+    // Add emissive on top
+    vec3 emissive = SRGBtoLinear(texture(tEmissive, vUv)).rgb * uEmissive;
+    color += emissive;
+    // Convert to sRGB to display
+    FragColor.rgb = linearToSRGB(color);
+    
+    // Apply uAlpha uniform at the end to overwrite any specular additions on transparent surfaces
+    FragColor.a = alpha * uAlpha;
+}
+`;
+
+export default {vertex, fragment};
--- src/webgl/world.js
+++ src/webgl/world.js
@@ -1,40 +1,95 @@
-import {Renderer, Geometry, Program, Color, Mesh} from 'ogl/src/Core.js'
-import TriangleScreenShader from '../shaders/TriangleScreenShader.js'
+import {Renderer, Transform, Camera, Geometry, Texture, Program, Mesh, Vec3, Color} from 'ogl/src/Core.js';
+import {Orbit, Plane} from 'ogl/src/Extras.js';
+import PBRShader300 from '../shaders/PBRShader300.js';
 
 export default class World{
     constructor(context){
-        const renderer = new Renderer({
-            width: window.innerWidth,
-            height: window.innerHeight,
-        });
+        const renderer = new Renderer({dpr: 2});
         const gl = renderer.gl;
         document.body.appendChild(gl.canvas);
-        gl.clearColor(1, 1, 1, 1);
+        gl.clearColor(0.1, 0.1, 0.1, 1);
+        const camera = new Camera(gl, {fov: 35});
+        camera.position.set(2, 0.5, 3);
+        const controls = new Orbit(camera);
         function resize() {
             renderer.setSize(window.innerWidth, window.innerHeight);
+            camera.perspective({aspect: gl.canvas.width / gl.canvas.height});
         }
         window.addEventListener('resize', resize, false);
         resize();
-        // Triangle that covers viewport, with UVs that still span 0 > 1 across viewport
-        const geometry = new Geometry(gl, {
-            position: {size: 3, data: new Float32Array([-1, -1, 0, 3, -1, 0, -1, 3, 0])},
-            uv: {size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2])},
-        });
-        const program = new Program(gl, {
-            vertex: TriangleScreenShader.vertex,
-            fragment: TriangleScreenShader.fragment,
-            uniforms: {
-                uTime: {value: 0},
-                uColor: {value: new Color(0.3, 0.2, 0.5)}
-            },
-        });
-        const mesh = new Mesh(gl, {geometry, program});
+        const scene = new Transform();
+        scene.position.y = -0.4;
+        const textureCache = {};
+        function getTexture(src, generateMipmaps = true) {
+            if (textureCache[src]) return textureCache[src];
+            const texture = new Texture(gl, {generateMipmaps});
+            const image = new Image();
+            textureCache[src] = texture;
+            image.onload = () => {
+                texture.image = image;
+            };
+            image.src = src;
+            return texture;
+        }
+        // Get fallback shader for WebGL1 - needed for OES_standard_derivatives ext
+        const Shader = PBRShader300;
+        loadExterior();
+        async function loadExterior() {
+            const data = await (await fetch(`assets/pbr/car-ext.json`)).json();
+
+            const geometry = new Geometry(gl, {
+                position: {size: 3, data: new Float32Array(data.position)},
+                uv: {size: 2, data: new Float32Array(data.uv)},
+                normal: {size: 3, data: new Float32Array(data.normal)},
+            });
+
+            // This whole effect lives in the fairly epic shader.
+            const program = new Program(gl, {
+                vertex: Shader.vertex,
+                fragment: Shader.fragment,
+                uniforms: {
+                    // Base color / albedo. This is used to determine both the diffuse and specular colors.
+                    tBaseColor: {value: getTexture('assets/pbr/car-ext-color.jpg')},
+                    // This works as a multiplier for each channel in the texture above.
+                    uBaseColor: {value: new Color(1, 1, 1)},
+
+                    // 'Roughness', 'Metalness' and 'Occlusion', each packed into their own channel (R, G, B)
+                    tRMO: {value: getTexture('assets/pbr/car-ext-rmo.jpg')},
+                    // The following are multipliers to the above values
+                    uRoughness: {value: 1},
+                    uMetallic: {value: 1},
+                    uOcclusion: {value: 1},
+
+                    // Just a regular normal map
+                    tNormal: {value: getTexture('assets/pbr/car-ext-normal.jpg')},
+                    uNormalScale: {value: 0.5},
+                    uNormalUVScale: {value: 1},
+
+                    // Emissive color is added at the very end to simulate light sources.
+                    tEmissive: {value: getTexture('assets/pbr/car-ext-emissive.jpg')},
+                    uEmissive: {value: 1},
+
+                    // uAlpha is an overall alpha control. It is applied right at the end to hide the geometry.
+                    // Specular reflections will not affect this value, unlike above.
+                    uAlpha: {value: 1},
+
+                    // One light is included, ideally to simulate the sun, and both specular and diffuse are calculated.
+                    uLightDirection: {value: new Vec3(0, 1, 1)},
+                    // Here I've pushed the white light beyond 1 to increase its effect.
+                    uLightColor: {value: new Vec3(7)},
+                },
+                transparent: true,
+            });
+            const mesh = new Mesh(gl, {geometry, program});
+            mesh.setParent(scene);
+        }
+
         requestAnimationFrame(update);
-        function update(t) {
+        function update() {
             requestAnimationFrame(update);
-            program.uniforms.uTime.value = t * 0.001;
-            // Don't need a camera if camera uniforms aren't required
-            renderer.render({scene: mesh});
+            scene.rotation.y += 0.005;
+            controls.update();
+            renderer.render({scene, camera});
         }
     }
 }
