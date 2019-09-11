95a58c460da0821188c72fc64035fc03a7bbc0da
joey-squla <joey@squla.com>
2019-09-11 17:35:52 +0200

    Added audio starting on interaction

--- src/components/AudioHost.vue
+++ src/components/AudioHost.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="audio-host">audioHost</div>
+    <div id="audio-host"></div>
 </template>
 
 <script>
@@ -10,6 +10,9 @@
 
         created () {
             window.addEventListener('wheel', this.onScroll);
+            window.addEventListener('mousedown', this.onMouseClick);
+
+            this.audioActive = false;
 
         },
         destroyed () {
@@ -23,34 +26,46 @@
                 var newGain =  this.prevGain - scrollVal;
                 this.prevGain = newGain;
                 this.fmGain.gain.value = Math.max(0, Math.min(2,newGain));
+            },
+            onMouseClick(){
+                if(!this.audioActive) this.startAudioContext();
+                this.audioActive = true;
+            },
+            startAudioContext(){
+                var noise = new Tone.Noise("pink").start();
+                noise.volume.value = 0.1;
+                var filter = new Tone.Filter(100, "lowpass");
+                noise.connect(filter);
+
+                var pingPong = new Tone.Chorus(0.001, 2.5, 0.7);
+                filter.connect(pingPong);
+
+                var gain = new Tone.Gain(0.3).toMaster();
+                pingPong.connect(gain);
+
+                //FM hummmm synth
+                var fmSynth = new Tone.FMSynth();
+                fmSynth.envelope.release = 1;
+                this.fmGain = new Tone.Gain(0.7);
+
+                fmSynth.connect(this.fmGain);
+                fmSynth.triggerAttack("C2");
+
+                var filter = new Tone.Filter(80, "lowpass").toMaster();
+                this.fmGain.connect(filter);
+
+                this.prevGain = this.fmGain.gain.value;
+
+                //Occasional beep synth
+
+
+
+
             }
         },
 
         mounted(){
-            var noise = new Tone.Noise("pink").start();
-            noise.volume.value = 0.1;
-            var filter = new Tone.Filter(100, "lowpass");
-            noise.connect(filter);
 
-            var pingPong = new Tone.Chorus(0.001, 2.5, 0.7);
-            filter.connect(pingPong);
-
-            var gain = new Tone.Gain(0.3).toMaster();
-            pingPong.connect(gain);
-
-            //FM HUMM SYNTH
-
-            var fmSynth = new Tone.FMSynth();
-            fmSynth.envelope.release = 1;
-            this.fmGain = new Tone.Gain(0.7);
-
-            fmSynth.connect(this.fmGain);
-            fmSynth.triggerAttack("C2");
-
-            var filter = new Tone.Filter(80, "lowpass").toMaster();
-            this.fmGain.connect(filter);
-
-            this.prevGain = this.fmGain.gain.value;
 
 
         }
