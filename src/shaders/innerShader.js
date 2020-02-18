5ed931b94797c08becb9028d8033ea4897278ba1
joey-squla <joey@squla.com>
2020-02-18 13:36:36 +0100

    Fixed shaders for mobile - still need square image for skybox

--- src/shaders/innerShader.js
+++ src/shaders/innerShader.js
@@ -1,16 +1,16 @@
-const vertex = `#version 300 es
+const vertex = `
 precision highp float;
 precision highp int;
 
-in vec3 position;
-in vec2 uv;
-in vec3 normal;
+attribute vec3 position;
+attribute vec2 uv;
+attribute vec3 normal;
 
 uniform mat3 normalMatrix;
 uniform mat4 modelMatrix;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
-out vec2 vUv;
+varying vec2 vUv;
 
 void main() {
     vUv = uv;
@@ -19,15 +19,14 @@ void main() {
 }
 `;
 
-const fragment = `#version 300 es
+const fragment = `
 precision highp float;
 precision highp int;
 
-in vec2 vUv;
-out vec4 FragColor;
+varying vec2 vUv;
 
 void main() {
-    FragColor = vec4(0.83,0.37,0.07, 1.);
+    gl_FragColor = vec4(0.83,0.37,0.07, 1.);
 }
 `;
 
--- src/webgl/world.js
+++ src/webgl/world.js
@@ -26,14 +26,14 @@ export default class World{
         this.scene.position.y = -0;
         this.textureCache = {};
         this.texturesLoaded = 0;
-        this.textureAmount = 5;
+        this.textureAmount = 4; //bad bad decision TODO: move this
 
         this.fftData = [];
 
         this.InnerShader = InnerShader;
         
         this.loadComet();
-        this.loadSkydome();
+        // this.loadSkydome();
         this.update = requestAnimationFrame(() => {this.updateLoop()});
     }
     toggle(active){
@@ -110,15 +110,43 @@ export default class World{
 
 
 
-        //NOTES:
-        /*
-            I needed to flip all the maps in photoshop to make them allign with the model in the end.
-         */
-        // This whole effect lives in the fairly epic shader.
+        this.program = this.renderer.isWebgl2 ? this.getProgram300() : this.getProgram100();
 
-        this.program = new Program(this.gl, {
-            vertex: this.renderer.isWebgl2 ? PBRShader300.vertex : PBRShader100.vertex,
-            fragment: this.renderer.isWebgl2 ? PBRShader300.fragment : PBRShader100.fragment,
+        this.cometMesh = new Mesh(this.gl, {geometry:this.geometry, program:this.program});
+
+        this.cometMesh.setParent(this.scene);
+
+        this.innerProgram = new Program(this.gl, {
+            vertex: this.InnerShader.vertex,
+            fragment: this.InnerShader.fragment,
+            transparent: true,
+        });
+
+        this.innerCometMesh = new Mesh(this.gl, {geometry:this.innerGeometry, program:this.innerProgram});
+        this.innerCometMesh.scale = new Vec3(0.5, 0.5, 0.5);
+        this.innerCometMesh.setParent(this.scene);
+    }
+
+    updateLoop() {
+        this.update = requestAnimationFrame(() => {this.updateLoop()});
+        this.scene.rotation.z += 0.0005;
+        this.scene.rotation.y += 0.0003;
+        this.controls.update();
+        this.renderer.render({scene:this.scene, camera:this.camera});
+    }
+
+    setFFT(fft){
+        for(var i=0;i<this.fftData.length;i++){
+            this.fftData[i] = fft[i%fft.length];
+        }
+        this.geometry.attributes.fft.data = new Float32Array(this.fftData);
+        this.geometry.attributes.fft.needsUpdate = true;
+    }
+
+    getProgram100(){
+        return new Program(this.gl, {
+            vertex: PBRShader100.vertex,
+            fragment:PBRShader100.fragment,
             uniforms: {
                 // Base color / albedo. This is used to determine both the diffuse and specular colors.
                 tBaseColor: {value: this.getTexture('assets/cometPBR/comet2_DefaultMaterial_BaseColor2.png')},
@@ -152,35 +180,50 @@ export default class World{
             },
             transparent: true,
         });
+    }
 
-        this.cometMesh = new Mesh(this.gl, {geometry:this.geometry, program:this.program});
+    getProgram300(){
+        //NOTES:
+        /*
+            I needed to flip all the maps in photoshop to make them allign with the model in the end.
+            WebGL2 -> make sure both shaders work to test WebGL1 look at Renderer instantiation webgl version parameter
+         */
 
-        this.cometMesh.setParent(this.scene);
+        return new Program(this.gl, {
+            vertex: PBRShader300.vertex,
+            fragment:PBRShader300.fragment,
+            uniforms: {
+                // Base color / albedo. This is used to determine both the diffuse and specular colors.
+                tBaseColor: {value: this.getTexture('assets/cometPBR/comet2_DefaultMaterial_BaseColor2.png')},
+                // This works as a multiplier for each channel in the texture above.
+                uBaseColor: {value: new Color(1, 1, 1)},
 
-        this.innerProgram = new Program(this.gl, {
-            vertex: this.InnerShader.vertex,
-            fragment: this.InnerShader.fragment,
+                // 'Roughness', 'Metalness' and 'Occlusion', each packed into their own channel (R, G, B)
+                tRMO: {value: this.getTexture('assets/cometPBR/comet2_DefaultMaterial_OcclusionRoughnessMetallic2.png')},
+                // The following are multipliers to the above values
+                uRoughness: {value: 0.3},
+                uMetallic: {value: 0.1},
+                uOcclusion: {value: 1},
+
+                // Just a regular normal map
+                tNormal: {value: this.getTexture('assets/cometPBR/comet2_DefaultMaterial_Normal2.png')},
+                uNormalScale: {value: 1},
+                uNormalUVScale: {value: 1},
+
+                // Emissive color is added at the very end to simulate light sources.
+                tEmissive: {value: this.getTexture('assets/cometPBR/comet2_DefaultMaterial_Emissive2.png')},
+                uEmissive: {value: 6},
+
+                // uAlpha is an overall alpha control. It is applied right at the end to hide the geometry.
+                // Specular reflections will not affect this value, unlike above.
+                uAlpha: {value: 1},
+
+                // One light is included, ideally to simulate the sun, and both specular and diffuse are calculated.
+                uLightDirection: {value: new Vec3(0, 1, 1)},
+                // Here I've pushed the white light beyond 1 to increase its effect.
+                uLightColor: {value: new Vec3(0.6)},
+            },
             transparent: true,
         });
-
-        this.innerCometMesh = new Mesh(this.gl, {geometry:this.innerGeometry, program:this.innerProgram});
-        this.innerCometMesh.scale = new Vec3(0.5, 0.5, 0.5);
-        this.innerCometMesh.setParent(this.scene);
-    }
-
-    updateLoop() {
-        this.update = requestAnimationFrame(() => {this.updateLoop()});
-        this.scene.rotation.z += 0.0005;
-        this.scene.rotation.y += 0.0003;
-        this.controls.update();
-        this.renderer.render({scene:this.scene, camera:this.camera});
-    }
-
-    setFFT(fft){
-        for(var i=0;i<this.fftData.length;i++){
-            this.fftData[i] = fft[i%fft.length];
-        }
-        this.geometry.attributes.fft.data = new Float32Array(this.fftData);
-        this.geometry.attributes.fft.needsUpdate = true;
     }
 }
