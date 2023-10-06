b3257b725cae3399289690cfba453243d0fc1b9a
Joey <hobscure@h2839196.stratoserver.net>
2023-10-06 10:44:34 +0200

    Added new Games

--- src/components/subPages/Games.vue
+++ src/components/subPages/Games.vue
@@ -2,6 +2,18 @@
 <div class="info">
     <div class="info"><h1>Games</h1></div>
     <div class="info">
+        <h2>Sammy's Toys</h2>
+        <iframe frameborder="0" src="https://itch.io/embed/2041871" width="552" height="167"><a href="https://hobscure.itch.io/sammys-toys">Sammy's Toys by hobscure</a></iframe>
+        <p>Sammy the cat lost his toys. Can you help him find them? Those toys can be heavy and you are not strong enough to jump while holding one.</p>
+    </div>
+
+    <div class="info">
+        <h2>Hybrid Magic Gone Wrong</h2>
+        <iframe frameborder="0" src="https://itch.io/embed/334854" width="552" height="167"><a href="https://hobscure.itch.io/hybrid-magic">Hybrid Magic - gone wrong by hobscure</a></iframe>
+        <p>You had been warned by the Warlich Warlock Society and even the Wugonian Wizards not to fool around with hybrid magic. But ... that idea! That bright idea of combining ice and fire magic into an amazing Hybrid Magic. You couldn't resist it any longer. It lingered and pressed upon your curiosity like a million pounds of Galgonium dead matter - that horrible urgance for you - out of all wizards - to be the one to make this a reality.
+          A bittersweet reality so it seems now. For the Hybrid Magic Gone Wrong! Made for Github Gamejam 2018</p>
+    </div>
+    <div class="info">
         <h2>Immelman</h2>
         <video loop muted controls autoplay>
             <source src="assets/videos/immelman.mp4" type="video/mp4">
